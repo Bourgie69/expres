e4f765c4807ab2da7ea16465af6622028921cc28
25LP4789 25LP4789 <25LP4789@Setups-iMac.local>
2025-11-05 11:26:29 +0800

    1

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors"
 import { userRouter } from "./src/routes/users.js";
 import { foodRouter } from "./src/routes/food.js";
 import { ordersRouter } from "./src/routes/orders.js";
@@ -12,6 +13,8 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
+
+app.use(cors())
 
 app.use("/users", userRouter);
 
--- src/controllers/users/delete-user.js
+++ src/controllers/users/delete-user.js
@@ -3,6 +3,6 @@ import { User } from "../../model/user-model.js";
 export const deleteUser = async (req, res) => {
     const id = req.body.id
     const deletedUser = await User.findByIdAndDelete(id)
-
+    
     res.status(200).json(deletedUser)
 };
