287ca9406469b74e207487beb0a221f8892dad44
25LP4789 25LP4789 <25LP4789@Setups-iMac.local>
2025-11-11 12:31:06 +0800

    1

--- src/model/food-model.js
+++ src/model/food-model.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
 const { Schema, model } = mongoose;
+import "./foodCategory-model.js";
 
 const foodSchema = new Schema({
   name: { type: String, required: true, default: "No name" },
-  price: { type: Number, required: true, default: "No price" },
+  price: { type: Number, default: "No price" },
   image: [{ type: String, default: null }],
   ingredients: { type: String, default: "No ingredients" },
-  category: { type: Schema.ObjectId, default: null, ref: "FoodCategory" },
+  category: { type: Schema.ObjectId, default: null, ref: "foodCategory" },
   createdAt: { type: Date, default: Date.now() },
   updatedAt: { type: Date, default: Date.now() },
 });
